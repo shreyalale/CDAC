ad81bb11fab04da2a9065828b825c73a73b70bab
Shreya Lale <98416771+shreyalale@users.noreply.github.com>
2024-12-27 00:51:13 +0530

    Add files via upload

---
+++ Mode8_C#/Assignment/Assignment_2/frequency/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+ 
+        int[] numbers = { 5, 9, 1, 5, 9, 5 };
+
+        var frequency = from num in numbers
+                        group num by num into grouped
+                        select new { Number = grouped.Key, Count = grouped.Count() };
+
+        Console.WriteLine("The number and the Frequency are:");
+        foreach (var item in frequency)
+        {
+            Console.WriteLine($"Number {item.Number} appears {item.Count} times");
+        }
+    }
+}
