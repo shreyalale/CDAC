bfc4a87c8a00a8bc3367e424828deab419fa286c
Shreya Lale <98416771+shreyalale@users.noreply.github.com>
2024-12-27 01:05:27 +0530

    Add files via upload

---
+++ Mode8_C#/Assignment/Assignment_2/produceRemainder/produceRemainder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class produceRemainder 
+{
+    static void Main() 
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+       
+        var query = from num in numbers
+                        where num % 2 == 0
+                        select num;
+
+        Console.WriteLine("The numbers which produce a remainder of 0 when divided by 2 are:");
+        foreach (int num in query)
+        {
+            Console.WriteLine(num + " ");
+        }
+    }
+}
---
+++ Mode8_C#/Assignment/Assignment_2/stringFrequency/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input the string:");
+        string input = Console.ReadLine();
+
+        var frequency = from ch in input
+                        group ch by ch into grouped
+                        select new { Character = grouped.Key, Count = grouped.Count() };
+
+        Console.WriteLine("The Frequency of characters are:");
+        foreach (var item in frequency)
+        {
+            Console.WriteLine($"Character {item.Character}: {item.Count} time {(item.Count > 1 ? "s" : "")}");
+        }
+    }
+}
