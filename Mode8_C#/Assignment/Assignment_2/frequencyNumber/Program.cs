bb74150b6a444144256971ad53e24f8d32d45c32
Shreya Lale <98416771+shreyalale@users.noreply.github.com>
2024-12-27 00:54:32 +0530

    Add files via upload

---
+++ Mode8_C#/Assignment/Assignment_2/frequencyNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+       
+        int[] numbers = { 9, 8, 6, 5 };
+
+     
+        var query = from num in numbers
+                    select new { Number = num, SqrNo = num * num };
+
+        Console.WriteLine("The numbers and their squares are:");
+        foreach (var item in query)
+        {
+            Console.WriteLine($"{{ Number = {item.Number}, SqrNo = {item.SqrNo} }}");
+        }
+    }
+}
