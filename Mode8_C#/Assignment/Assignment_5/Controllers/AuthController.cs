005bcf74d23579358301048e15738e863e5c383d
Shreya Lale <98416771+shreyalale@users.noreply.github.com>
2024-12-28 00:06:34 +0530

    Add files via upload

---
+++ Mode8_C#/Assignment/Assignment_5/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using userRegister.Models;
+using userRegister.Service;
+
+namespace userRegister.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserService _userService;
+
+        public AuthController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (_userService.Validate(user.Email, user.Password))
+                return RedirectToAction("Index", "Home");
+            ModelState.AddModelError("", "Invalid email or password");
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            _userService.Register(user);
+            return RedirectToAction("Login");
+        }
+    }
+}
